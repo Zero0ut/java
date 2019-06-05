c5a6faa47424a5efc647c4dbe0e8423c5d8f9545
Alex <alex.kpc@gmail.com>
2019-06-05 23:38:09 +0800

    Improve the Healthcheck

--- 04_React/src/main/java/com/phoneix/projects/react/ReactApplication.java
+++ 04_React/src/main/java/com/phoneix/projects/react/ReactApplication.java
@@ -2,14 +2,20 @@ package com.phoneix.projects.react;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import reactor.core.publisher.Flux;
 
 @EnableMongoAuditing
 @EnableReactiveMongoRepositories
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class ReactApplication {
 
 	public static void main(String[] args) {
