7ee646f8b5ee1f4d1b2ceeb9572e08385358d262
zero0ut <alex.kpc@gmail.com>
2019-10-07 13:42:56 +0800

    Add Zuul

--- 08_Stock-viewer/stock-service/src/main/java/com/techprimers/stock/stockservice/StockServiceApplication.java
+++ 08_Stock-viewer/stock-service/src/main/java/com/techprimers/stock/stockservice/StockServiceApplication.java
@@ -2,9 +2,11 @@ package com.techprimers.stock.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StockServiceApplication {
 
--- 08_Stock-viewer/stock-service/src/main/java/com/techprimers/stock/stockservice/config/Config.java
+++ 08_Stock-viewer/stock-service/src/main/java/com/techprimers/stock/stockservice/config/Config.java
@@ -1,6 +1,6 @@
 package com.techprimers.stock.stockservice.config;
 
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Config {
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
