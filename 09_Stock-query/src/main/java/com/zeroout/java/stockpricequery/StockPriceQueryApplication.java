44541ecf4c085cb191c4443f58be2061abeeef8b
zero0ut <alex.kpc@gmail.com>
2019-12-05 22:08:09 +0800

    Add actuator endpoints

--- 09_Stock-query/src/main/java/com/zeroout/java/stockpricequery/StockPriceQueryApplication.java
+++ 09_Stock-query/src/main/java/com/zeroout/java/stockpricequery/StockPriceQueryApplication.java
@@ -2,15 +2,7 @@ package com.zeroout.java.stockpricequery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @EnableSwagger2
 @SpringBootApplication
